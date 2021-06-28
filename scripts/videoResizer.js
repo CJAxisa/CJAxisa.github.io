c78f1294b5d002c92cb775159293452c4cad089e
CJ Axisa <axisac72@gmail.com>
2021-06-28 13:38:58 -0400

    Moving into this repo

---
+++ scripts/videoResizer.js
@@ -0,0 +1,10 @@
+
+update();
+
+function update=()=>{
+    requestAnimationFrame(update);
+    let video = document.querySelector("#inSiteVideo");
+
+    if(video!=null)
+//        video.style.height= (video.style.width*9);
+};
